3ae6cfa4a4687b578d9d9202e1fd01872e06ef11
gioz75 <kartik75@gmail.com>
2016-05-31 12:16:51 +0930

    Identified two features that needs to be implemented...
    Place holders are in place...

--- HelloJavaDemo/src/com/hpe/devops/HelloJavaDemo.java
+++ HelloJavaDemo/src/com/hpe/devops/HelloJavaDemo.java
@@ -2,6 +2,20 @@ package com.hpe.devops;
 
 public class HelloJavaDemo {
 	public static void main(String args[]){
-		System.out.println("Hello Java!");
+
+		/**
+		 * 		Provide disclaimer		 *		 
+		 * 
+		 * */
+		
+		
+		System.out.println("Hello!");
+		
+		/**
+		 * 		Interactive Greetings...
+		 * 
+		 * */
+		
+		
 	}
 }
